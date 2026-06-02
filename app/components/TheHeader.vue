6a2fed3bdfeb1280f46378ff365b2cbc526ea824
Fayzu1in <sherikf1@gmail.com>
2026-06-02 13:08:13 +0500

    feat: create navbar components and install nuxt swiper

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <HeaderTop class="header-top" />
+
+    <HeaderMain class="header-main" />
+
+    <HeaderCategories class="header-categories" />
+  </header>
+</template>
